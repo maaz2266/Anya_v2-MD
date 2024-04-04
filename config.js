18efa89c25e0437c1c46eb198d0605d11b7c7d65
maaz2266 <156607236+maaz2266@users.noreply.github.com>
2024-04-04 12:05:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ module.exports = {
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
   ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
+  ownernumber: process.env.Owner_Number || "33644624722",
   instagramId: process.env.Insta || "8.08_only_mine",
   warns: process.env.Warns_Limits || 3,
   mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
